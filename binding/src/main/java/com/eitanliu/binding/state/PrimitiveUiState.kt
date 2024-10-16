83e12edb70a8841023d27bf668b52fa1d49052ce
eitanliu <eitanliu@gmail.com>
2024-10-16 10:17:39 +0800

    typealias

--- binding/src/main/java/com/eitanliu/binding/state/PrimitiveUiState.kt
+++ binding/src/main/java/com/eitanliu/binding/state/PrimitiveUiState.kt
@@ -3,23 +3,25 @@ package com.eitanliu.binding.state
 // class PrimitiveUiState
 
 typealias SingleUiState<T> = SingleLiveState<T>
-typealias SingleUiStateBoolean = SingleUiState<Boolean>
-typealias SingleUiStateByte = SingleUiState<Byte>
-typealias SingleUiStateChart = SingleUiState<Char>
-typealias SingleUiStateShort = SingleUiState<Short>
-typealias SingleUiStateInt = SingleUiState<Int>
-typealias SingleUiStateLong = SingleUiState<Long>
-typealias SingleUiStateFloat = SingleUiState<Float>
-typealias SingleUiStateDouble = SingleUiState<Double>
-typealias SingleUiStateEnum<T> = SingleUiState<T>
+typealias PrimitiveSingleUiState<T> = PrimitiveSingleLiveState<T>
+typealias SingleUiStateBoolean = PrimitiveSingleLiveState<Boolean>
+typealias SingleUiStateByte = PrimitiveSingleLiveState<Byte>
+typealias SingleUiStateChart = PrimitiveSingleLiveState<Char>
+typealias SingleUiStateShort = PrimitiveSingleLiveState<Short>
+typealias SingleUiStateInt = PrimitiveSingleLiveState<Int>
+typealias SingleUiStateLong = PrimitiveSingleLiveState<Long>
+typealias SingleUiStateFloat = PrimitiveSingleLiveState<Float>
+typealias SingleUiStateDouble = PrimitiveSingleLiveState<Double>
+typealias SingleUiStateEnum<T> = PrimitiveSingleLiveState<T>
 
 typealias MultipleUiState<T> = MultipleLiveState<T>
-typealias MultipleUiStateBoolean = MultipleUiState<Boolean>
-typealias MultipleUiStateByte = MultipleUiState<Byte>
-typealias MultipleUiStateChart = MultipleUiState<Char>
-typealias MultipleUiStateShort = MultipleUiState<Short>
-typealias MultipleUiStateInt = MultipleUiState<Int>
-typealias MultipleUiStateLong = MultipleUiState<Long>
-typealias MultipleUiStateFloat = MultipleUiState<Float>
-typealias MultipleUiStateDouble = MultipleUiState<Double>
-typealias MultipleUiStateEnum<T> = MultipleLiveState<T>
+typealias PrimitiveMultipleUiState<T> = PrimitiveMultipleLiveState<T>
+typealias MultipleUiStateBoolean = PrimitiveMultipleLiveState<Boolean>
+typealias MultipleUiStateByte = PrimitiveMultipleLiveState<Byte>
+typealias MultipleUiStateChart = PrimitiveMultipleLiveState<Char>
+typealias MultipleUiStateShort = PrimitiveMultipleLiveState<Short>
+typealias MultipleUiStateInt = PrimitiveMultipleLiveState<Int>
+typealias MultipleUiStateLong = PrimitiveMultipleLiveState<Long>
+typealias MultipleUiStateFloat = PrimitiveMultipleLiveState<Float>
+typealias MultipleUiStateDouble = PrimitiveMultipleLiveState<Double>
+typealias MultipleUiStateEnum<T> = PrimitiveMultipleLiveState<T>
